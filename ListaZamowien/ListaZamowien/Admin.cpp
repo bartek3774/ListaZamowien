df984f22edd370f60f03fce4642f2dede6249c21
Bartłomiej Stępień <Bartek@DESKTOP-BARTEK>
2018-01-29 19:46:03 +0100

    Aktualizacja

--- ListaZamowien/ListaZamowien/Admin.cpp
+++ ListaZamowien/ListaZamowien/Admin.cpp
@@ -14,6 +14,7 @@ Admin::Admin(SingIn log1)
 void Admin::startPage()
 {
 	cout << "Witaj " << SingIn::login << endl;
+	cout << "Typ konta: " << SingIn::accountType << endl;
 	cout << "1. Zarzadzanie uzytkownikami\n2. Dodaj zamowienie\n3. Lista wszystkich zamowien\n4. Lista zamowien uzytkownika\n5. Wyjdz" << endl;
 	string x;
 	cin >> x;
@@ -24,15 +25,24 @@ void Admin::startPage()
 	}
 	else if (x == "2")
 	{
-
+		system("cls");
+		addOrder();
+		startPage();
 	}
 	else if (x == "3")
 	{
-
+		system("cls");
+		printOrder();
+		startPage();
 	}
 	else if (x == "4")
 	{
-
+		system("cls");
+		string id;
+		cout << "Podaj login uzytkownika" << endl;
+		cin >> id;
+		printOrder(id);
+		startPage();
 	}
 	else if (x == "5")
 	{
@@ -54,15 +64,21 @@ void Admin::startPage(int x)
 		cin >> x2;
 		if (x2 == "1")
 		{
-
+			system("cls");
+			printUsers();
+			startPage(1);
 		}
 		else if (x2 == "2")
 		{
-
+			system("cls");
+			addUser();
+			startPage(1);
 		}
 		else if (x2 == "3")
 		{
-
+			system("cls");
+			removeUser();
+			startPage(1);
 		}
 		else if (x2 == "4")
 		{
@@ -75,6 +91,174 @@ void Admin::startPage(int x)
 			startPage(1);
 		}
 	}
+}
+
+void Admin::addUser()
+{
+
+	string id, name, surname;
+
+	cout << "Podaj login uzytkownika" << endl;
+	cin >> id;
+	cout << "Podaj imie uzytkownika" << endl;
+	cin >> name;
+	cout << "Podaj nazwisko uzytkownika" << endl;
+	cin >> surname;
+
+	ofstream usersFile;
+	usersFile.open("users.txt", ios::app);//ID NAME SURNAME PASSWORD USER
+	usersFile << id + " " + name + " " + surname + " " + id+"h" + " Uzytkownik" << endl;
+	usersFile.close();
+	system("cls");
+	cout << "Konto utworzone" << endl;
+
+}
+
+int Admin::checkUser(string id)
+{
+	ifstream usersFile;
+	usersFile.open("users.txt", ios::app);
+	string loginTmp, nameTmp, surnameTmp, passwordTmp, accountTypeTmp;
+	while (!usersFile.eof())
+	{
+		
+		usersFile >> loginTmp >> nameTmp >> surnameTmp >> passwordTmp >> accountTypeTmp;
+		
+		if (accountTypeTmp == "Uzytkownik" && loginTmp == id)
+		{
+			usersFile.close();
+			return 1;
+		}
+	}
+	usersFile.close();
+	return 0;
+}
+
+void Admin::removeUser()
+{
+	string id, x;
+	cout << "Podaj numer uzytkownika" << endl;
+	cin >> id;
+	cout << "Czy napewno chcesz trwale usunac uzytkownika o id: " << id << " (t/n)" << endl;
+	cin >> x;
+	if (x == "t")
+	{
+		ifstream usersFile;
+		usersFile.open("users.txt", ios::app);
+
+		int i = 1;
+		while (!usersFile.eof())
+		{
+			string loginTmp, nameTmp, surnameTmp, passwordTmp, accountTypeTmp;
+			usersFile >> loginTmp >> nameTmp >> surnameTmp >> passwordTmp >> accountTypeTmp;
+			if (loginTmp == id && accountTypeTmp == "Uzytkownik")
+			{
+				usersFile.close();
+				deleteRow("users.txt", i);
+				system("cls");
+				cout << "Uzytkownik usuniety" << endl;
+				startPage(1);
+				break;
+			}
+			i++;
+		}
+		system("cls");
+		cout << "Brak uzytkownika o tym id" << endl;
+	}
+	else
+	{
+		system("cls");
+		cout << "Blad usuwania uzytkownika" << endl;
+	}
+}
+
+void Admin::printUsers()
+{
+	ifstream usersFile;
+	usersFile.open("users.txt", ios::app);
+	cout << "Uzytkownicy" << endl;
+	cout << endl << "-----------------------------------------------------------------------------------------------------------" << endl;
+	while (!usersFile.eof())
+	{
+		string loginTmp, nameTmp, surnameTmp, passwordTmp, accountTypeTmp;
+		usersFile >> loginTmp >> nameTmp >> surnameTmp >> passwordTmp >> accountTypeTmp;
+		if (accountTypeTmp == "Uzytkownik")
+		{
+			cout << loginTmp << " " << nameTmp << " " << surnameTmp << endl;
+		}
+	}
+	usersFile.close();
+	cout << "-----------------------------------------------------------------------------------------------------------" << endl << endl << endl;
+}
+
+void Admin::addOrder()
+{
+	int id = idGenerator();
+	string login, item;
+	int quantity;
+	float price;
+	cout << "Podaj login uzytkownika" << endl;
+	cin >> login;
+	int x;
+	if (x = checkUser(login))
+	{
+		cout << "Podaj nazwe towaru" << endl;
+		cin >> item;
+		cout << "Podaj ilosc sztuk" << endl;
+		cin >> quantity;
+		cout << "Podaj cene sztuki" << endl;
+		cin >> price;
+		ofstream ordersFile;
+		ordersFile.open("orders.txt", ios::app);//ID LOGIN ITEM QUANTITY PRICE PRICE_SUM
+		ordersFile << id << " " << login + " " + item + " " << quantity << " " << price << " " << quantity * price << endl;
+		ordersFile.close();
+		system("cls");
+		cout << "Zamowienie dodane" << endl;
+	}
+	else
+	{
+		system("cls");
+		cout << "Nie ma takiego uzytkownika" << endl;
+	}
+}
+
+void Admin::printOrder()
+{
+	ifstream ordersFile;
+	ordersFile.open("orders.txt", ios::app);
+	cout << "Zamowienia" << endl;
+	cout << endl << "-----------------------------------------------------------------------------------------------------------" << endl;
+	cout << "Numer zamowienia Login Przedmiot Ilosc Cena jednostkowa Wartosc" << endl;
+	float sum = 0;
+	int x = 0;
+	while (!ordersFile.eof())
+	{
+		string idTmp, loginTmp, itemTmp;
+		int quantityTmp;
+		float priceTmp, priceSumTmp;
+		ordersFile >> idTmp >> loginTmp >> itemTmp >> quantityTmp >> priceTmp >> priceSumTmp;
+		if (idTmp.length() > 1)
+		{
+			cout << idTmp << " " << loginTmp << " " << itemTmp << " " << quantityTmp << "szt " << priceTmp << "zl " << priceSumTmp << "zl" << endl;
+			sum += priceSumTmp;
+			x++;
+		}
+	}
+	ordersFile.close();
+	cout << "-----------------------------------------------------------------------------------------------------------" << endl ;
+	cout << "Laczna kwota zamowien: " << sum << "zl." << endl;
+	cout << "Laczna liczba zamowien: " << x << endl << endl << endl;
+}
+
+void Admin::printOrder(string id)
+{
+}
+
+int Admin::idGenerator()
+{
+		time_t czas;
+		srand(time(&czas));
+		return rand() % 5000 + 1;
 }
 
 void Admin::deleteRow(string path, size_t row)
--- ListaZamowien/ListaZamowien/Admin.h
+++ ListaZamowien/ListaZamowien/Admin.h
@@ -6,6 +6,14 @@ public:
 	Admin(SingIn log);
 	void startPage();
 	void startPage(int x);
+	void addUser();
+	int checkUser(string id);
+	void removeUser();
+	void printUsers();
+	void addOrder();
+	void printOrder();
+	void printOrder(string id);
+	int idGenerator();
 
 	static void deleteRow(string path, size_t row);
 	Admin();
